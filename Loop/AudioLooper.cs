8202e3c8139f29ba2fc9b8510646f9edd6bac155
Herman Tulleken <herman.tulleken@gmail.com>
2023-08-11 16:30:35 -0400

    Fixed some bugs

--- Loop/AudioLooper.cs
+++ Loop/AudioLooper.cs
@@ -1,6 +1,50 @@
-﻿namespace Loop;
+﻿using NAudio.Wave;
 
-public class AudioLooper
+namespace Loop;
+
+public abstract class AudioLooper
 {
+	public abstract void MakeLoopable(string inputMp3File, string outputMp3File, int fadeTime);
+}
+
+public abstract class AudioLooper<TWaveFormat> : AudioLooper
+	where TWaveFormat : WaveFormat
+{
+	public override void MakeLoopable(string inputMp3File, string outputMp3File, int fadeTime)
+	{
+		var (sampleData, waveFormat) = Load(inputMp3File);
+		float[] crossfadedData = Crossfade(sampleData, waveFormat.SampleRate, fadeTime);
+		float[] trimmedData = Trim(crossfadedData, sampleData.Length / 2);
+		Save(outputMp3File, trimmedData, waveFormat);
+	}
 	
+	protected abstract  (float[], TWaveFormat)  Load(string inputPath );
+	protected abstract  void Save(string outputPath, float[] trimmedData, TWaveFormat waveFormat);
+	
+	private static float[] Crossfade(float[] sampleData, int sampleRate, int fadeTime)
+	{
+		int fadeSampleCount = 2 * (fadeTime * sampleRate / 1000);
+		float[] crossfadedData = new float[sampleData.Length * 2 - fadeSampleCount];
+		Array.Copy(sampleData, crossfadedData, sampleData.Length - fadeSampleCount);
+		Array.Copy(sampleData, fadeSampleCount, crossfadedData, sampleData.Length, sampleData.Length - fadeSampleCount);
+
+		for (int i = 0; i < fadeSampleCount; i++)
+		{
+			float fadeInFactor = i / (float) fadeSampleCount;
+			float fadeOutFactor = 1 - fadeInFactor;
+			
+			crossfadedData[sampleData.Length - fadeSampleCount + i] 
+				= sampleData[i] * fadeInFactor 
+				  + sampleData[sampleData.Length - fadeSampleCount + i] * fadeOutFactor;
+		}
+
+		return crossfadedData;
+	}
+
+	private static float[] Trim(float[] crossfadedData, int samplesToSkip)
+	{
+		float[] trimmedData = new float[crossfadedData.Length - 2 * samplesToSkip];
+		Array.Copy(crossfadedData, samplesToSkip, trimmedData, 0, trimmedData.Length);
+		return trimmedData;
+	}
 }
--- Loop/Program.cs
+++ Loop/Program.cs
@@ -1,18 +1,14 @@
-﻿using NAudio.Lame;
-using NAudio.Wave;
-using static System.Console;
+﻿using static System.Console;
 
 namespace Loop;
 
 /**
 	A simply quick-and-dirty program to make MP3s and WAVs loopable, following the following rough process:
 	
-	1. Read the input file into a byte array
-	2. Convert the byte array to a float array
-	3. Crossfade the float array with itself
-	4. Trim the float array so that the beginning and end match up. 
-	5. Convert the float array back to a byte array
-	6. Write the byte array to the output file
+	1. Read the input file into a float array
+	2. Crossfade the float array with itself
+	3. Trim the float array so that the beginning and end match up. 
+	4. Write the float array to the output file
 */
 public static class Program
 {
@@ -27,8 +23,7 @@ Arguments:
 
 	private const int DefaultCrossfadeDurationInMilliseconds = 1000;
 	private const string DefaultInputFile = "input.mp3";
-	private const int Max16BitInt = 32768;
-	private const float Max16BitIntAsFloat = Max16BitInt;
+
 	public static void Main(string[] args)
 	{
 		if (args.Contains("-h"))
@@ -39,7 +34,7 @@ Arguments:
 
 		int fadeTime = ParseFadeTimeFromArgs(args);
 
-		// Let's assume the file is the first argument if it doesn't start with a hyphen
+		// Assume the file is the first argument if it doesn't start with a hyphen
 		// Check if a file argument was provided
 		string inputFilePath = args.Length > 0
 			? !args[0].StartsWith("-") ? args[0] : DefaultInputFile
@@ -49,20 +44,14 @@ Arguments:
 		string fileExtension = Path.GetExtension(inputFilePath).ToLower();
 		string outputFilePath = Path.GetFileNameWithoutExtension(inputFilePath) + "-loop" + fileExtension;
 
-		switch (fileExtension)
+		AudioLooper looper = fileExtension switch
 		{
-			case ".mp3":
-				MakeMp3sLoopable(inputFilePath, outputFilePath, fadeTime);
-				break;
-
-			case ".wav":
-				MakeWavsLoopable(inputFilePath, outputFilePath, fadeTime);
-				break;
-
-			default:
-				WriteLine("Unsupported file format.");
-				break;
-		}
+			".mp3" => new Mp3Looper(),
+			".wav" => new WaveLooper(),
+			_ => throw new Exception($"Unsupported file extension: {fileExtension}")
+		};
+		
+		looper.MakeLoopable(inputFilePath, outputFilePath, fadeTime);
 	}
 
 	private static void PrintUsage() => WriteLine(UsageMessage);
@@ -80,102 +69,5 @@ Arguments:
 		}
 
 		return DefaultCrossfadeDurationInMilliseconds;
-	}
-
-
-	// Now, modify the MakeMp3sLoopable and MakeWavsLoopable methods to accept the input and output file paths as parameters:
-	// ReSharper disable once InconsistentNaming
-	private static void MakeMp3sLoopable(string inputMp3File, string outputMp3File, int fadeTime)
-	{
-		using var reader = new Mp3FileReader(inputMp3File);
-
-		var byteList = new List<byte>();
-		int bytesRead;
-		byte[] buffer = new byte[reader.Mp3WaveFormat.AverageBytesPerSecond * 4];
-		
-		while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
-		{
-			byteList.AddRange(buffer.Take(bytesRead));
-		}
-
-		float[] sampleData = ByteToFloat(byteList);
-		float[] crossfadedData = Crossfade(sampleData, reader.Mp3WaveFormat.SampleRate, fadeTime);
-		float[] trimmedData = Trim(crossfadedData);
-		var outputData = FloatToByte(trimmedData);
-		
-		var outputStream = new MemoryStream();
-		using (var writer = new LameMP3FileWriter(outputStream, reader.Mp3WaveFormat, LAMEPreset.STANDARD))
-		{
-			writer.Write(outputData.ToArray(), 0, outputData.Count);
-		}
-
-		File.WriteAllBytes(outputMp3File, outputStream.ToArray());
-	}
-
-	private static List<byte> FloatToByte(float[] trimmedData)
-	{
-		var outputData = new List<byte>();
-		foreach (float sample in trimmedData)
-		{
-			outputData.AddRange(BitConverter.GetBytes((short)(sample * Max16BitInt)));
-		}
-
-		return outputData;
-	}
-
-	private static float[] ByteToFloat(List<byte> byteList)
-	{
-		float[] sampleData = new float[byteList.Count / 2];
-		
-		for (int i = 0, j = 0; i < byteList.Count; i += 2, j++)
-		{
-			sampleData[j] = BitConverter.ToInt16(byteList.ToArray(), i) / Max16BitIntAsFloat;
-		}
-
-		return sampleData;
-	}
-
-	private static void MakeWavsLoopable(string inputWavFile, string outputWavFile, int fadeTime)
-	{
-		using var reader = new AudioFileReader(inputWavFile);
-		// Convert the reader to a SampleProvider for easier manipulation
-		var samples = reader.ToSampleProvider();
-
-		// Load all samples into a buffer
-		float[] sampleData = new float[(int)(reader.Length / sizeof(float))];
-		samples.Read(sampleData, 0, sampleData.Length);
-		float[] crossfadedData = Crossfade(sampleData, reader.WaveFormat.SampleRate, fadeTime);
-		float[] trimmedData = Trim(crossfadedData);
-
-		// Save to output file
-		using var writer = new WaveFileWriter(outputWavFile, reader.WaveFormat);
-		writer.WriteSamples(trimmedData, 0, trimmedData.Length);
-	}
-	
-	private static float[] Crossfade(float[] sampleData, int sampleRate, int fadeTime)
-	{
-		int fadeSampleCount = fadeTime * sampleRate / 1000;
-		float[] crossfadedData = new float[sampleData.Length * 2 - fadeSampleCount];
-		Array.Copy(sampleData, crossfadedData, sampleData.Length);
-		Array.Copy(sampleData, 0, crossfadedData, sampleData.Length, sampleData.Length - fadeSampleCount);
-
-		for (int i = 0; i < fadeSampleCount; i++)
-		{
-			float fadeOutFactor = 1 - (i / (float) fadeSampleCount);
-			float fadeInFactor = i / (float) fadeSampleCount;
-			crossfadedData[sampleData.Length + i] 
-				= sampleData[i] * fadeInFactor 
-				  + crossfadedData[sampleData.Length - fadeSampleCount + i] * fadeOutFactor;
-		}
-
-		return crossfadedData;
-	}
-
-	private static float[] Trim(float[] crossfadedData)
-	{
-		int samplesToSkip = crossfadedData.Length / 4;
-		float[] trimmedData = new float[crossfadedData.Length - 2 * samplesToSkip];
-		Array.Copy(crossfadedData, samplesToSkip, trimmedData, 0, trimmedData.Length);
-		return trimmedData;
 	}
 }
